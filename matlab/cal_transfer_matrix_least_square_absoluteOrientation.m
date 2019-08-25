cafcead4e9ba53eb09313b1ac75f188ab9354433
ysong-aceinna <ysong@aceinna.com>
2019-08-25 10:45:58 +0800

    8-22 V2_03_difficult test

--- matlab/cal_transfer_matrix_least_square_absoluteOrientation.m
+++ matlab/cal_transfer_matrix_least_square_absoluteOrientation.m
@@ -25,14 +25,15 @@ algo_pose_z = data(:,10);
 % load('EuRoC_MH_02_easy.mat')
 [row,clum] = size(ref_pose_x);
 
-% for "machine_hall_02", n = 200;
-% for "vicon_room_1_02", n = 100;
-% for "vicon_room_1_03", n = 100;
+% for "MH_02_easy", n = 200;
+% for "V1_02_medium", n = 100;
+% for "V2_03_difficult", n = 100;
 
-% for "machine_hall_02", n = 120;
-% for "vicon_room_1_02", n = 90;
+% for "MH_02_easy", n = 120;
+% for "V1_02_medium", n = 90;
+% for "V2_03_difficult", n = 90;
 
-n =120; %取ref_pose 和 algo_pose 的前n个坐标点，求转换矩阵。
+n =90; %取ref_pose 和 algo_pose 的前n个坐标点，求转换矩阵。
 rx = ref_pose_x(1:n,:);
 ry = ref_pose_y(1:n,:);
 rz = ref_pose_z(1:n,:);
